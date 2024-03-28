6225292dbb3e955ba5b337ba2ad4b8b4071eb4b4
chlee-rdv <cheolhee.lee@farmily.ai>
2024-03-28 10:38:01 +0900

    Update configuration and commenting out publishers
    Added new parameters 'pcTopic' and 'imgTopic' to the configuration YAML file and related code. The publishers for 'cv_ptr->image' and 'cv_ptr->toImageMsg()' have been commented out in lidar_camera_node.cpp. Also, previous hard-coded values for 'pcTopic' and 'imgTopic' in launch file have been commented out to reflect the new changes in program behaviour.

--- src/lidar_camera_node.cpp
+++ src/lidar_camera_node.cpp
@@ -385,9 +385,9 @@ int main(int argc, char** argv)
 	nh.getParam("/ang_Y_resolution", angular_resolution_y);
 
 	nh.getParam("/matrix_file/pcTopic", pcTopic);
+	ROS::INFO("Point Cloud Topic: %s", pcTopic.c_str());
 	nh.getParam("/matrix_file/imgTopic", imgTopic);
-
-	std::cout << pcTopic << ", " << imgTopic << std::endl;
+	ROS::INFO("Image Topic: %s", imgTopic.c_str())
 
 	XmlRpc::XmlRpcValue param;
 	nh.getParam("/matrix_file/camera_matrix", param);
