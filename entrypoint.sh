c178670bb1aef2cd3c6975a09e90ca75d5f38580
chlee-rdv <cheolhee.lee@farmily.ai>
2024-04-02 19:10:44 +0900

    config file 정리 / 멀티 실행 런치 파일 추가

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+export ROS_MASTER_URI=http://192.168.101.100:11311
+export ROS_IP=192.168.101.100
+export ROS_HOSTNAME=$(hostname)
+
 source /opt/ros/noetic/setup.bash
+
 cd /root/catkin_ws && catkin_make
 source /root/catkin_ws/devel/setup.bash
+
 exec "$@"
